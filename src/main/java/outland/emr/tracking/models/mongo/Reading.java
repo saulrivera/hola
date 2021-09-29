f0ef8d28dd0f7842c8df0d95947ae734d400c18f
Saul Rivera <saulriveraa@outlook.com>
2021-09-28 21:56:40 -0500

    fix: Jackson DateTimeDeserializer

--- src/main/java/outland/emr/tracking/models/mongo/Reading.java
+++ src/main/java/outland/emr/tracking/models/mongo/Reading.java
@@ -3,6 +3,8 @@ package outland.emr.tracking.models.mongo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.joda.deser.DateTimeDeserializer;
 import org.joda.time.DateTime;
 import org.springframework.data.annotation.Id;
 import outland.emr.tracking.models.Detection;
@@ -15,6 +17,7 @@ public class Reading extends Detection {
     @Id
     private String id = UUID.randomUUID().toString();
     @JsonProperty("timestamp")
+    @JsonDeserialize(using = DateTimeDeserializer.class)
     private DateTime timestamp;
     @JsonProperty("rssi")
     private double rssi;
