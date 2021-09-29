72984e87334ee260f530d596570bab0aefff693d
Saul Rivera <saulriveraa@outlook.com>
2021-09-29 14:14:19 -0500

    fix

--- src/main/java/outland/emr/tracking/schedulers/BroadcastStreamScheduler.java
+++ src/main/java/outland/emr/tracking/schedulers/BroadcastStreamScheduler.java
@@ -51,7 +51,7 @@ public class BroadcastStreamScheduler {
         streamManager.clearStreamStack();
     }
 
-    @Scheduled(fixedRate = 2500)
+    @Scheduled(fixedRate = 5000)
     public void flushThreads() {
         threadManager.flushThreads();
     }
