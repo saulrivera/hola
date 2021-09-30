51e4291e89854c77f8f2a46324135d94b66464ff
Saul Rivera <saulriveraa@outlook.com>
2021-09-29 20:09:50 -0500

    fix: increasin livetime threads

--- src/main/java/outland/emr/tracking/schedulers/BroadcastStreamScheduler.java
+++ src/main/java/outland/emr/tracking/schedulers/BroadcastStreamScheduler.java
@@ -51,7 +51,7 @@ public class BroadcastStreamScheduler {
         streamManager.clearStreamStack();
     }
 
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRate = 60000)
     public void flushThreads() {
         threadManager.flushThreads();
     }
